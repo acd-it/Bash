1c9e1f1b2b4f39c8d8a7041784bfaf24853518e4
ACD <75489343+acd-it@users.noreply.github.com>
2024-10-14 11:24:11 -0400

    Create RenamemacOS.sh

---
+++ RenamemacOS.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## ACD - 6/6/23
+## Rename One Machine Script ##
+
+## This script is for IT Admins to manually use to rename one machine. This can helpful
+## when a user has more than one machine, is using a loaner, or we want a machine to have a specific name.
+
+
+# Replace with machine name
+username="computername"
+
+# Set HostNames
+scutil --set HostName $username
+scutil --set LocalHostName $username
+scutil --set ComputerName $username
+
+#Run a recon to JAMF
+sudo jamf recon
